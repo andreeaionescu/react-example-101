f6a6b26f8e7f6e747d4d650588a7e2f22e046732
Andreea Ionescu <amionescu15@gmail.com>
2018-08-26 18:51:56 +0100

    Clean-up files from create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import { MuiThemeProvider } from 'material-ui/styles';
