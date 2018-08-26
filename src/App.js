e07f77089c98fd0bdb8567a8f7bfa524edb0442b
Andreea Ionescu <amionescu15@gmail.com>
2018-08-26 18:47:56 +0100

    Removed React pre-built configuration.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Grid container>
+          <Grid item xs={3}>
+            <Paper elevation={4}>
+              <div>{'Inside the paper'}</div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from 'material-ui/Grid'
+
+export default class Menu extends React.Component{
+
+    render(){
+        return (
+        <Grid>
+
+        </Grid> 
+    )
+    }
+}
