355d7732de200904803a942e57cf1947b9bb112e
Andreea Ionescu <amionescu15@gmail.com>
2019-09-23 17:36:51 +0100

    Welcome includes all steps for sing up and sign in

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,64 +1,128 @@
 import React from 'react'
 import {Input, Button, Form, Header} from "semantic-ui-react"
-import {Auth} from 'aws-amplify'
 
-export default class Login extends React.Component {
-
-    constructor(props){
-        super(props)
-        this.state = {
-            username: '',
-            password: ''
-        }
-
-        this.handleOnSubmit = this.handleOnSubmit.bind(this)
-        this.setUsername = this.setUsername.bind(this)
-        this.setPassword = this.setPassword.bind(this)
-    }
-
-    handleOnSubmit(e){
-        e.preventDefault()
-        console.log('Submitted!')
-        console.log('Username: ', this.state.username)
-        console.log('Password: ', this.state.password)
-        this.setState({username: '', password: ''})
-        e.target.reset()
-    }
-
-    setUsername(e){
-        this.setState({username: e.target.value})
-    }
-
-    setPassword(e){
-        this.setState({password: e.target.value})
-    }
+class SignUp extends React.Component {
 
     render(){
         return(
-            <Form onSubmit={this.handleOnSubmit}>
-                <Header size="huge">Login to continue</Header>
+            <Form>
+                <Header size="huge">Sign up to continue</Header>
                 <Form.Group>
                     <Form.Field
                         control={Input}
+                        name='username'
                         label='Username'
                         type='text'
                         placeholder='Type your username here..'
                         width={12}
-                        onChange={this.setUsername}
+                        onChange={this.props.handleOnChange}
                     />
                 </Form.Group>
                 <Form.Group>
                     <Form.Field
                         control={Input}
+                        name='password'
                         label='Password'
                         type='password'
                         placeholder='Type your password here..'
                         width={12}
-                        onChange={this.setPassword}
+                        onChange={this.props.handleOnChange}
                     />
                 </Form.Group>
-                <Button type='submit'>Submit</Button>
+                <Form.Group>
+                    <Form.Field
+                        control={Input}
+                        name='email'
+                        label='Email'
+                        type='text'
+                        placeholder='Type your email here..'
+                        width={12}
+                        onChange={this.props.handleOnChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field
+                        control={Input}
+                        name='phone_number'
+                        label='Phone number'
+                        type='text'
+                        placeholder='Type your phone number here..'
+                        width={12}
+                        onChange={this.props.handleOnChange}
+                    />
+                </Form.Group>
+                <Button type='submit' onClick={this.props.singUp}>Submit</Button>
             </Form>
         )
     }
 }
+
+class ConfirmSignUp extends React.Component {
+    render(){
+        return(
+            <Form>
+                <Header size="huge">Confirm sign up </Header>
+                <Form.Group>
+                    <Form.Field
+                        control={Input}
+                        name='username'
+                        label='Username'
+                        type='text'
+                        placeholder='Type your username here..'
+                        width={12}
+                        onChange={this.props.handleOnChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field
+                        control={Input}
+                        name='authenticationCode'
+                        label='Authentication code'
+                        type='text'
+                        placeholder='Type authentication code here..'
+                        width={12}
+                        onChange={this.props.handleOnChange}
+                    />
+                </Form.Group>
+                <Button type='submit' onClick={this.props.confirmSignUp}>Confirm Sign Up</Button>
+            </Form>
+        )
+    }   
+}
+
+class Login extends React.Component {
+
+    render(){
+        return (
+        <Form>
+                <Header size="huge">Login to continue</Header>
+                <Form.Group>
+                    <Form.Field
+                        control={Input}
+                        name='username'
+                        label='Username'
+                        type='text'
+                        placeholder='Type your username here..'
+                        width={12}
+                        onChange={this.props.handleOnChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field
+                        control={Input}
+                        name='password'
+                        label='Password'
+                        type='password'
+                        placeholder='Type your password here..'
+                        width={12}
+                        onChange={this.props.handleOnChange}
+                    />
+                </Form.Group>
+                <Button type='submit' onClick={this.props.signIn}>Submit</Button>
+            </Form>
+    )
+}
+
+}
+
+export {Login, SignUp, ConfirmSignUp}
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,9 +1,88 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
-import Login from './Login';
-
+import { Grid, Image, Header } from 'semantic-ui-react';
+import { Login, SignUp, ConfirmSignUp } from './Login';
+import {Auth} from 'aws-amplify'
 
 export default class Welcome extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+            phone_number: '',
+            authenticationCode: '',
+            step: 0,
+            existingUser: null,
+        }
+    }
+
+    handleOnChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    signUp = async () => {
+        const {username, password, email, phone_number } = this.state
+        try {
+            await Auth.signUp({ username, password, attributes: {email, phone_number}})
+            console.log('successfully signed up!')
+            this.setState({step: 1})
+        } catch (err) {
+            console.log('error signing up: ', err)
+            if (err.code === "UsernameExistsException") {
+                this.setState({existingUser: true, errMessage: err.message})
+            }
+        }
+    }
+
+    confirmSignUp = async () => {
+        const { username, authenticationCode } = this.state
+        try {
+            await Auth.confirmSignUp(username, authenticationCode)
+            console.log('user succesfully signed up!')
+            this.setState({step: 2})
+        } catch (err) {
+            console.log('error confirming sing up: ', err)
+        }
+    }
+
+    signIn = async () => {
+        const { username, password } = this.state
+        try {
+            await Auth.signIn(username, password)
+            console.log('user succesfully signed in!')
+            this.setState({signedIn: true})
+        } catch (err) {
+            console.log('error logging in: ', err)
+        }
+    }
+
+    clickSignIn = () => {
+        this.setState({existingUser: false})
+    }
+
+    clickLogIn = () => {
+        this.setState({existingUser: true})
+    }
+
+    displaySignUpOptions = () => {
+        switch(this.state.step){
+            case 0:
+                return <SignUp singUp={this.signUp} handleOnChange={this.handleOnChange}/>
+                break;
+            case 1:
+                return <ConfirmSignUp confirmSignUp={this.confirmSignUp} handleOnChange={this.handleOnChange}/>
+                break;
+            default:
+                return <Login signIn={this.signIn} handleOnChange={this.handleOnChange}/>
+        }
+    }
+
+    displayBasedOnUserType = (existingUser) => {
+        if (!existingUser) return
+        return existingUser===true ? <Login signIn={this.signIn} handleOnChange={this.handleOnChange}/> : this.displaySignUpOptions()
+    }
 
     render(){
         return (
@@ -12,7 +91,8 @@ export default class Welcome extends React.Component {
                     <Image centered size='large' src='src/Images/code_girls.png' />
                 </Grid.Column>
                 <Grid.Column>
-                    <Login/>
+                    <Header>Welcome to this awesome authentication page using AWS Amplify and React!</Header>
+                    {this.state.existingUser ? <Login signIn={this.signIn} handleOnChange={this.handleOnChange}/> : this.displaySignUpOptions(this.state.step)}    
                 </Grid.Column>
             </Grid>
         )
